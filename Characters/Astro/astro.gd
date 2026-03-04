0f7cd666d5cd26c3c6e3d07786e2426f90ea94fe
Troublinggem <jnelson335@ivytech.edi>
2026-03-04 17:06:42 -0500

    RESTARING

--- Characters/Astro/astro.gd
+++ Characters/Astro/astro.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 class_name AstroClassName
 @export var current_tool: data_types.Tools = data_types.Tools.None
-@onready var inventory: Inventory = $Inventory
 
 var player_direction: Vector2
--- ControlNodes/UI/Inventory and Hotbar/item.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-
-class_name Item
-
-@export var name: String
-@export var icon: Texture2D
-@export var max_stack: int = 99
